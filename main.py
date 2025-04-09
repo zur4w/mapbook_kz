dae6be632aa1e020427f9e9b7ee77a110c470005
zur4w <zurawskikamil42@gmail.com>
2025-04-09 11:19:54 +0200

    wip: Add new user functionality

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from utils.model import users
-from utils.controller import get_user_info
+from utils.controller import get_user_info, add_user
 
 def main():
     print(f'Witaj {users[0]['name']}')
@@ -13,6 +13,7 @@ while True:
        choice: str = input('Wybierz opcje menu: ')
        if choice == '0': break
        if choice == '1': get_user_info(users[1:])
+       if choice == '2': add_user(users)
 
 
 if __name__ == '__main__':
